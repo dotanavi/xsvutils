ac7ae59f4b6d08d02a2e12c435f5c671840d694b
Ota Daichi <daichi.ota@naviplus.co.jp>
2019-07-01 14:38:25 +0900

    Update Rust version to 2018

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,6 @@
+use std::env;
 use std::io;
 use std::io::BufWriter;
-
-#[macro_use]
-extern crate lazy_static;
-extern crate memchr;
-extern crate regex;
-extern crate url;
-
-use std::env;
-
 #[macro_use]
 mod util;
 mod command;
